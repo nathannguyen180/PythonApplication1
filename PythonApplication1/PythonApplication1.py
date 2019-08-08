6064dbb76324b4f27d74d60b638508639cdc76ad
Nathan Nguyen <nathannguyen180@gmail.com>
2019-08-08 15:48:26 -0400

    there is no such thing as a multi-line comment

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,7 +1,9 @@
+
+
 some_message = "wtf, wheres string or int?"
 some_number_variable = 5
 quick_mafs = 2+2-1+some_number_variable
-
+#?????????????????????????????????????????????????????????????????
 QUICK_MAFS = 22/7
 
 my_wallet = 480
@@ -12,11 +14,68 @@ python_sub += real_tax_math
 my_wallet -= python_sub
 
 print()
-print(my_wallet)
+print(int(my_wallet))
 print()
 
 print (some_message)
 print (some_number_variable)
 
-print(QUICK_MAFS)
+print(int(QUICK_MAFS))
 print (quick_mafs)
+# why is this a sharp sign instead of // or /**/ or anything else, rEEEEEEEEEEEE
+
+so_this_is_a_f_l_o_a_t = 524526.
+
+print()
+
+cucumbers = 100 
+num_people = 6
+
+quotient1 = cucumbers/num_people
+# the value of quotient1 is 3.5
+print(quotient1)
+print()
+whole_cucumbers_per_person = float(cucumbers)/num_people
+
+print (whole_cucumbers_per_person)
+
+
+not_haiku = '''
+The old pond,
+A frog jumps in:
+Plop!
+
+'''
+print(not_haiku)
+
+
+a = True
+b = False
+wowowowowowowowowow
+c = true
+d = false
+
+   
+
+age = 15
+print ("I am " + str(age) + " years old!")
+
+
+print()
+print()
+print()
+print()
+print()
+
+number1 = "100"
+number2 = "10"
+
+string_addition = number1 + number2 
+#string_addition now has a value of "10010"
+
+print(string_addition)
+
+int_addition = int(number1) + int(number2)
+#int_addition has a value of 110
+
+print(int_addition)
