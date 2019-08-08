fc7726236e8a6c953afe2c47c86484017480b68f
Nathan Nguyen <nathannguyen180@gmail.com>
2019-08-08 14:05:06 -0400

    Add project files.

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,22 @@
+some_message = "wtf, wheres string or int?"
+some_number_variable = 5
+quick_mafs = 2+2-1+some_number_variable
+
+QUICK_MAFS = 22/7
+
+my_wallet = 480
+python_sub = 201
+real_tax_math = 0.08 * python_sub
+
+python_sub += real_tax_math
+my_wallet -= python_sub
+
+print()
+print(my_wallet)
+print()
+
+print (some_message)
+print (some_number_variable)
+
+print(QUICK_MAFS)
+print (quick_mafs)
