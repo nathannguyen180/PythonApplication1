18b2b0c2e3c1ebe5787a49533963a3a0b4d26b61
Nathan Nguyen <nathannguyen180@gmail.com>
2019-08-09 12:07:35 -0400

    IT HAS BEEN DONE.txt

--- PythonApplication1/module1.py
+++ PythonApplication1/module1.py
@@ -1,6 +1,35 @@
-import shutil
+import shutil, os
+from os.path import join#, getsize
 
-src = "C:\\Users\\NNguyen\\Desktop\\old Csharp.txt"
-dst = "C:\\lol"
+#C:\\Users\\NNguyen\\Desktop\\old Csharp.txt
+src = ""
+dst = "C:\\lol\\biglol"
+top = "C:\\Users\\NNguyen\\Desktop"
 
-shutil.copy2(src,dst)
+
+
+#os.path.join(top, "Basic Calculator.txt")
+
+print()
+
+
+
+for theDirPath, theDirNames, theFileNames in os.walk(top):
+    #print(theDirPath, "consumes", end=" ")
+    #print(sum(getsize(join(theDirPath, name)) for name in theFileNames), end=" ")
+    #print("bytes in", len(theFileNames), "non-directory files\n")
+    
+    for file in theFileNames:
+        if file.endswith(".txt"):
+            src = join(theDirPath, file)
+            print(src)
+            shutil.copy2(src,dst)
+
+            
+
+#    if 'Microsoft' in dirs:
+#        dirs.remove('Microsoft')  # don't visit these directories
+#    if 'dotnet' in dirs:
+#        dirs.remove('dotnet') 
+#    if 'Android' in dirs:
+#        dirs.remove('Android') 
