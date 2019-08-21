1364213493ebfbcd1143f3fc5e8e7610444570a9
Nathan Nguyen <nathannguyen180@gmail.com>
2019-08-21 16:08:26 -0400

    final

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -271,3 +271,76 @@ print (some_dict['list_1'])
 
 
 
+menu = {} # Empty dictionary
+menu['Chicken Alfredo'] = 14.50 # Adding new key-value pair
+print menu['Chicken Alfredo']
+
+# Your code here: Add some dish-price pairs to menu!
+menu['Chicken'] = 25.60
+menu['rice'] = 30.20
+menu['salad'] = 10.99
+
+print "There are " + str(len(menu)) + " items on the menu."
+print menu
+
+
+
+# key - animal_name : value - location 
+zoo_animals = { 'Unicorn' : 'Cotton Candy House',
+'Sloth' : 'Rainforest Exhibit',
+'Bengal Tiger' : 'Jungle House',
+'Atlantic Puffin' : 'Arctic Exhibit',
+'Rockhopper Penguin' : 'Arctic Exhibit'}
+# A dictionary (or list) declaration may break across multiple lines
+
+# Removing the 'Unicorn' entry. (Unicorns are incredibly expensive.)
+del zoo_animals['Unicorn']
+
+# Your code here!
+del zoo_animals["Sloth"]
+del zoo_animals["Bengal Tiger"]
+
+zoo_animals["Rockhopper Penguin"] = 'DC'
+
+print zoo_animals
+
+
+
+backpack = ['xylophone', 'dagger', 'tent', 'bread loaf']
+backpack.remove("dagger")
+
+
+
+
+
+
+
+
+
+inventory = {
+  'gold' : 500,
+  'pouch' : ['flint', 'twine', 'gemstone'], # Assigned a new list to 'pouch' key
+  'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']
+}
+
+# Adding a key 'burlap bag' and assigning a list to it
+inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
+
+# Sorting the list found under the key 'pouch'
+inventory['pouch'].sort() 
+
+# Your code here
+inventory['pocket'] = ['seashell', 'strange berry', 'lint']
+
+inventory["backpack"].sort()
+
+inventory["backpack"].remove('dagger')
+
+inventory['gold'] += 50
+
+print (inventory['gold'])
+
+
+#https://www.codecademy.com/learn/learn-python
+
+
